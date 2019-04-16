e144791d44e73f36bb7ce024a82c8a8ceab4598a
Glenn Streiff <gsnospampls@pobox.com>
2019-04-16 10:46:16 -0500

    add calculated portions of model

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,5 +1,5 @@
 function Controller() {
-    this.model = new Model();
+    this.model = new Model(Model.glennDbConfig);
     this.addSubmitHandler();
 }
 
@@ -28,16 +28,19 @@ Controller.prototype.getData = function() {
     console.log("model data = ", this.model.getData());
 }
 
-function Model() {
+function Model(dbConfig) {
+    this.dbConfig = dbConfig;
     this.dbInit();
-    this.dbRef = this.getdbRef();
+    this.dbRef = this.getDbRef();
     this.addDbListener('child_added');
 }
 Model.prototype.name = "";
 Model.prototype.startDate = "";
 Model.prototype.rate = "";
 Model.prototype.role = "";
-Model.prototype.dbConfig = {
+Model.prototype.calcData = {"monthsWorked": 0, "totalPay": 0};
+Model.prototype.dbConfig = {};
+Model.prototype.glennDbConfig = {
     apiKey: "AIzaSyAPIUI8WY6dJaqFj9yVCVeGOE4f_XLCw6E",
     authDomain: "employee-billing.firebaseapp.com",
     databaseURL: "https://employee-billing.firebaseio.com",
@@ -45,12 +48,14 @@ Model.prototype.dbConfig = {
     storageBucket: "employee-billing.appspot.com",
     messagingSenderId: "528459595368"
 };
+
 Model.prototype.setData = function(name, startDate, rate, role) {
     this.name = name;
     this.startDate = startDate;
     this.rate = rate;
     this.role = role;
 }
+
 Model.prototype.getData = function() {
     results = {};
     results.name = this.name;
@@ -59,12 +64,15 @@ Model.prototype.getData = function() {
     results.role = this.role;
     return results;
 }
+
 Model.prototype.dbInit = function() {
     firebase.initializeApp(this.dbConfig);
 }
-Model.prototype.getdbRef = function(childNode) {
+
+Model.prototype.getDbRef = function(childNode) {
     return firebase.database().ref(childNode);
 }
+
 Model.prototype.dbPushRecord = function() {
     this.dbRef.push({
         "name": this.name,
@@ -73,25 +81,32 @@ Model.prototype.dbPushRecord = function() {
         "role": this.role
       });
 }
+
 Model.prototype.addDbListener = function(dbEvent = 'child_added') {
     let that = this;
     this.dbRef.limitToLast(1).on(dbEvent, function(childSnapshot) {
-        let employeeData = childSnapshot.val();
-        that.updateView(employeeData);
+        let empData = childSnapshot.val();
+        that.calcData = that.paySinceStartDate(empData.startDate, empData.rate);
+        that.updateView(empData);
     });
 }
-Model.prototype.updateView = function(data) {
+
+Model.prototype.updateView = function(dbData) {
     $("#employeeRecords").append(
-        `${data.name} ${data.startDate} ${data.rate} ${data.role}<br />`
+        `${dbData.name} ${dbData.startDate} ${dbData.rate} ${dbData.role} ${this.calcData.monthsWorked} ${this.calcData.totalPay}<br />`
     );
 }
 
-// let mdy = $("#start-date").val();
-// console.log("mdy = ", mdy);
-// var ts = moment(mdy, "M/D/YYYY").valueOf();
-// var currentDate = moment();
-// console.log("ts = ", ts);
-// console.log("currentDate = ", currentDate);
-// // var m = moment(ts);
-// // var s = m.format("M/D/YYYY H:mm");
-// // alert("Values are: ts = " + ts + ", s = " + s);
+Model.prototype.monthsWorked = function(mdyStartDate) {
+    let startDateMsec = moment(mdy, "M/D/YYYY").valueOf();
+    let currentDate = moment();
+    let monthsWorked = currentDate.diff(startDateMsec, 'month');
+    return monthsWorked;
+}
+
+Model.prototype.paySinceStartDate = function(startDate, monthlyRate) {
+    results = {}
+    results.monthsWorked = this.monthsWorked(startDate);
+    results.totalPay = parseFloat(results.monthsWorked) * parseFloat(monthlyRate);
+    return results;
+}
